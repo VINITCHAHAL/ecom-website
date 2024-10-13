6a6953786a40099b917b269724e7471ef09cbf3a
VINITCHAHAL <vinitchahal9797@gmail.com>
2024-10-13 18:07:20 +0530

    Add profile page and social platform links

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('login/',views.login,name='login'),
     path('register/',views.register,name='register'),
-    path('add/',views.add_person)
-    # path('about/',views.about,name='about'),
-
-]
+    path('add/',views.add_person),
+    path('profile/', views.profile, name='profile'), 
+    path('profile/<str:username>/', views.profile, name='profile'),
+   ]
