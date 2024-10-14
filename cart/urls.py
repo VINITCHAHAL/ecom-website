28611e208debc7094cf88d3f6fca486bc836ab0e
VINITCHAHAL <vinitchahal9797@gmail.com>
2024-10-14 21:40:41 +0530

    views added and also simple logout done

--- cart/urls.py
+++ cart/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('login/',views.login,name='login'),
-    path('register/',views.register,name='register'),
-    path('add/',views.add_person),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'), 
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'), 
     path('profile/<str:username>/', views.profile, name='profile'),
-   ]
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('ecom/', views.ecom, name='ecom'),
+]
